6e46559552b185f0ae498bd4913a24808fed5607
Jeremy Blum <jeremy@jeremyblum.com>
2014-07-13 01:59:06 -0700

    MySQL Database(s) backup and archival functionality added. Still needs backup purging.

--- Nodealyzer.py
+++ Nodealyzer.py
@@ -7,7 +7,7 @@
 # License: GPL v3 (http://www.gnu.org/licenses/gpl.html)
 
 #Import needed libraries
-import ConfigParser, pyzmail, base64, datetime, os, re, pprint
+import ConfigParser, pyzmail, base64, datetime, time, os, re, pprint, subprocess, gzip, sys, shutil, glob
 
 #Read configuration file
 config = ConfigParser.ConfigParser()
@@ -16,34 +16,157 @@ config.read("config.ini")
 #Main program execution
 def main():
 
+        print "\nNodealyzer is taking care of business! Stand by, this shouldn't take long...\n"
+
 	#Perform Preflight Checks
 	#print "Performing Preflight Checks..."
 	#TODO: Confirm valid values in the config file
 
 	#Get some initial info
 	#print "Gathering Info..."
+
+        #We'll fill in the email report body as we go
+        email_body = ""
 	
 	#Let's Backup some databases!
-	print "Backing up MySQL Databases..."
-	print backupDatabases()
-	
+	sys.stdout.write("Backing up MySQL Databases...")
+	[success, details] = backupDatabases()
+	if success:
+                print "Success!"
+        else:
+                print "Failures Occurred!"
+        print details
+        print ""
+
+        #Adds backups to the long-term archival folder if due for it
+        sys.stdout.write("Checking and Updating Backup Archives...")
+	[success, details] = updateArchives(int(config.get('MySQL','archival_frequency')))
+	if success:
+                print "Success!"
+        else:
+                print "Failures Occurred!"
+        print details
+        print ""
+        
 	#Send the email report
 	#print "Sending Email report...",
-	#print sendEmail()
+	#sys.stdout.write(sendEmail())
+
+        print "All Done!"
 
 #Backup the databases
 def backupDatabases():
-	pwd = os.path.dirname(os.path.realpath(__file__))
-	backup_dir = pwd + "/SQL_Backups/"
-	
-	#Iterate through each database that must be backed up
-	db_descriptions = re.compile('\s*,\s*').split(config.get('MySQL', 'descriptions'))
-	db_names        = re.compile('\s*,\s*').split(config.get('MySQL', 'names'))
-	db_hosts        = re.compile('\s*,\s*').split(config.get('MySQL', 'hosts'))
-	db_usernames    = re.compile('\s*,\s*').split(config.get('MySQL', 'usernames'))
-	db_passwords    = re.compile('\s*,\s*').split(config.get('MySQL', 'passwords'))
-	##for db in db_descriptions:
-	#TODO: Finish me
+	backup_dir = os.path.dirname(os.path.realpath(__file__)) + "/SQL_Backups/"
+
+	suffix = time.strftime('%Y-%m-%d_%H-%M-%S')
+
+	all_success = True
+	details = ""
+        #Iterate through the configured databases
+        for db in xrange(int(config.get('MySQL', 'num_databases'))):
+                #Setup database and backup variables
+                description = config.get('DB' + str(db+1), 'description')
+                name = config.get('DB' + str(db+1), 'name')
+                host = config.get('DB' + str(db+1), 'host')
+                username = config.get('DB' + str(db+1), 'username')
+                password = config.get('DB' + str(db+1), 'password')
+                this_backup_dir = backup_dir + name + "/"
+
+
+                #Create backup folder if needed
+                try:
+                        if not os.path.exists(this_backup_dir):
+                                os.makedirs(this_backup_dir)
+                except:
+                        details = details + "Error creating or finding backup directory for " + description + "\n"
+                        all_success = False
+                else:
+                        #Execute Database dump and gzip
+                        filename = name + "_" + suffix + ".sql.gz"
+                        cmd = ['mysqldump', '--add-drop-table', '-h', host, '--user', username, '--password='+password, name]
+                        p1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        dump_output, err = p1.communicate()
+                        if p1.returncode is not 0:
+                                details = details + err
+                                all_success = False
+                        else:
+                                try:
+                                        f = gzip.open(this_backup_dir + filename, "wb")
+                                        f.write(dump_output)
+                                        f.close()
+                                except:
+                                        details = details + "There was a problem writing " + description + " to the gzipped output file\n"
+                                        all_success = False
+                                else:
+                                        details = details + description + " was successfully dumped to " + this_backup_dir + filename + "\n"
+
+        details = details.strip('\n')       
+        return [all_success, details]
+
+#Clean old backups and add to the archive
+def updateArchives(archival_frequency):
+        backup_dir = os.path.dirname(os.path.realpath(__file__)) + "/SQL_Backups/"
+	all_success = True
+	details = ""
+        #Iterate through the database backup folders
+	backup_folders = sorted(os.listdir(backup_dir))
+	if not backup_folders:
+                details = details + "No database backups were found for archival.\n"
+        else:
+                for name in backup_folders:
+                        if os.path.isfile(name): pass
+                        else:
+                                archive_dir = backup_dir + name + "/archives/"
+                                #Create archive folder if needed and add to archive if appropriate
+                                try:
+                                        if not os.path.exists(archive_dir):
+                                                os.makedirs(archive_dir)
+                                except:
+                                        details = details + "Error creating or finding archive directory for SQL_Backups/" + name + "\n"
+                                        all_success = False
+                                else:
+                                        #Look through the archives and identify the latest archived file
+                                        archives = sorted(os.listdir(archive_dir))
+                                        archive_update_needed = False
+                                        if not archives:
+                                                archive_update_needed = True
+                                                details = details + "No previous archive of SQL_Backups/" + name + " found. "
+                                        else:
+                                                newest_archive = archive_dir + archives[-1]
+                                                if os.path.isfile(newest_archive):
+                                                        name_split = newest_archive.split("_")
+                                                        name_date = name_split[-2]
+                                                        name_time = name_split[-1].split('.')[0]
+                                                        file_creation_datetime = datetime.datetime.strptime(name_date + "_" + name_time, '%Y-%m-%d_%H-%M-%S')
+                                                        
+                                                        #If it's more than the desired days old, we should add the newest backup the archive
+                                                        if datetime.timedelta.total_seconds(datetime.datetime.now() - file_creation_datetime) > archival_frequency*24*60*60:
+                                                                archive_update_needed = True
+                                                                details = details + "Latest archive of SQL_Backups/" + name + " is more than " + str(archival_frequency) + " days old. "
+
+                                        if archive_update_needed:
+                                                #Find newest backup
+                                                
+                                                try:
+                                                        backups = sorted(glob.glob(backup_dir + name + "/*.sql.gz"))
+                                                        newest_backup = backups[-1]
+                                                        if os.path.isfile(newest_backup):
+                                                                shutil.copyfile(newest_backup, archive_dir + newest_backup.split("/")[-1])
+                                                        else:
+                                                                all_success = False
+                                                                details = details + "Suitable backup could not be found for Archival!\n"
+                                                except:
+                                                        all_success = False
+                                                        details = details + "Backup archival failed!\n"
+                                                else:
+                                                        details = details + "Latest backup archived successfully!\n"
+                                                        
+                                        else:
+                                                details = details + "SQL_Backups/" + name + " archives are up-to-date. No archive added.\n"   
+
+
+        details = details.strip('\n') 
+        return [all_success, details]
 	
 #Generate and send the Email
 def sendEmail():
