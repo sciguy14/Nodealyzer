03e2bc1098656efad840dbc7669de04d39f15e02
Jeremy Blum <jeremy@jeremyblum.com>
2014-07-27 17:55:17 -0700

    Added Email HTML generation for MySQL backups and archivals

--- Nodealyzer.py
+++ Nodealyzer.py
@@ -27,25 +27,34 @@ def main():
 
         #We'll fill in the email report body as we go
         email_body = ""
+
+        #DATABASES ---------------------------------
+        email_body = email_body + "<h1>Database Backup</h1>"
 	
 	#Let's Backup some databases!
 	sys.stdout.write("Backing up MySQL Databases...")
-	[success, details] = backupDatabases()
+	[success, details, html] = backupDatabases()
 	if success:
                 print "Success!"
+                email_body = email_body + "<p>The following MySQL databases were backed up successfully:</p>\n"
         else:
                 print "Failures Occurred!"
+                email_body = email_body + "<p>Errors occured while backing up MySQL databases:</p>\n"
         print details
+        email_body = email_body + html
         print ""
 
         #Adds backups to the long-term archival folder if due for it
         sys.stdout.write("Checking and Updating Backup Archives...")
-	[success, details] = updateArchives(int(config.get('MySQL','archival_frequency')))
+	[success, details, html] = updateArchives(int(config.get('MySQL','archival_frequency')))
 	if success:
                 print "Success!"
+                email_body = email_body + "<p>Long-term archives were successfully created if due:</p>\n"
         else:
                 print "Failures Occurred!"
+                email_body = email_body + "<p>Errors occured while checking or creating long-term archives:</p>\n"
         print details
+        email_body = email_body + html
         print ""
 
         #Delete Old Daily Backups
@@ -77,11 +86,14 @@ def main():
                 sys.stdout.write("Archives Configured to not Expire...")
                 print "Moving On."
         print ""
+
+        #END DATABASES -----------------------------
         
 	#Send the email report
-	#print "Sending Email report...",
-	#sys.stdout.write(sendEmail())
+	sys.stdout.write("Sending Email report...")
+	print sendEmail(email_body)
 
+        print ""
         print "All Done!"
 
 #Backup the databases
@@ -92,6 +104,7 @@ def backupDatabases():
 
 	all_success = True
 	details = ""
+	html = "<ul>\n"
         #Iterate through the configured databases
         for db in xrange(int(config.get('MySQL', 'num_databases'))):
                 #Setup database and backup variables
@@ -109,6 +122,7 @@ def backupDatabases():
                                 os.makedirs(this_backup_dir)
                 except:
                         details = details + "Error creating or finding backup directory for " + description + "\n"
+                        html = html + "\t<li>Error creating or finding backup directory for " + description + "</li>\n"
                         all_success = False
                 else:
                         #Execute Database dump and gzip
@@ -126,22 +140,27 @@ def backupDatabases():
                                         f.close()
                                 except:
                                         details = details + "There was a problem writing " + description + " to the gzipped output file\n"
+                                        html = html + "\t<li>There was a problem writing " + description + " to the gzipped output file</li>\n"
                                         all_success = False
                                 else:
                                         details = details + description + " was successfully dumped to SQL_Backups/" + name + "/" + filename + "\n"
+                                        html = html + "\t<li>" + description + " was successfully dumped to SQL_Backups/" + name + "/" + filename + "</li>\n"
 
-        details = details.strip('\n')       
-        return [all_success, details]
+        details = details.strip('\n')
+        html = html + "</ul>\n"
+        return [all_success, details, html]
 
 #Clean old backups and add to the archive
 def updateArchives(archival_frequency):
         backup_dir = os.path.dirname(os.path.realpath(__file__)) + "/SQL_Backups/"
 	all_success = True
 	details = ""
+	html = "<ul>\n"
         #Iterate through the database backup folders
 	backup_folders = sorted(os.listdir(backup_dir))
 	if not backup_folders:
                 details = details + "No database backups were found for archival.\n"
+                html = html + "\t<li>No database backups were found for archival.</li>\n"
         else:
                 for name in backup_folders:
                         if os.path.isfile(name): pass
@@ -153,6 +172,7 @@ def updateArchives(archival_frequency):
                                                 os.makedirs(archive_dir)
                                 except:
                                         details = details + "Error creating or finding archive directory for SQL_Backups/" + name + "\n"
+                                        html = html + "\t<li>Error creating or finding archive directory for SQL_Backups/" + name + "</li>\n"
                                         all_success = False
                                 else:
                                         #Look through the archives and identify the latest archived file
@@ -185,18 +205,24 @@ def updateArchives(archival_frequency):
                                                         else:
                                                                 all_success = False
                                                                 details = details + "Suitable backup could not be found for Archival!\n"
+                                                                html = html + "\t<li>Suitable backup for SQL_Backups/" + name + " could not be found for Archival!</li>\n"
+                                                                
                                                 except:
                                                         all_success = False
                                                         details = details + "Backup archival failed!\n"
+                                                        html = html + "\t<li>Backup archival for SQL_Backups/" + name + " failed!</li>\n"
                                                 else:
                                                         details = details + "Latest backup archived successfully!\n"
+                                                        html = html + "\t<li>Latest backup of SQL_Backups/" + name + " archived successfully!</li>\n"
                                                         
                                         else:
-                                                details = details + "SQL_Backups/" + name + " archives are up-to-date. No archive added.\n"   
+                                                details = details + "SQL_Backups/" + name + " archives are up-to-date. No archive added.\n"
+                                                html = html + "\t<li>SQL_Backups/" + name + " archives are up-to-date. No archive added.</li>\n"
 
 
-        details = details.strip('\n') 
-        return [all_success, details]
+        details = details.strip('\n')
+        html = html + "</ul>\n"
+        return [all_success, details, html]
 
 #Delete Expired Backups
 #backup_type = "daily" or "archive"
@@ -252,7 +278,7 @@ def deleteBackups(backup_type, num_to_maintain):
         
 	
 #Generate and send the Email
-def sendEmail():
+def sendEmail(body):
 
 	#encoded inline image
 	logo=base64.b64decode(	"""R0lGODlhMgAyAPcAAAAAAP///yCQkCCPjyKRkSSRkSaTkyaSkieUlCeTkyiUlCiTkymVlSmUlCqVlSqUlCuVlSyWliyVlS2Wli6Xly6Wli+XlzCYmC+WljCXlzGYmDKZmTKYmDOZmTSamjWbmzSZmTWamjabmzaamjebmzicnDqdnTqcnDudnTyenjydnT+fn0CgoEKhoUOiokKgoEOhoUSiokWjo0ShoUajo0Wiokaiokejo0mkpEqlpUqkpEulpUympk2mpk6np06mpk+np1Cnp1GoqFKpqVKoqFOpqVSqqlOoqFSpqVarq1WqqlaqqlisrFerq1msrFqtrVutrVyurl2urlytrV6vr1+wsF+vr16urmCwsGCvr2KxsWKwsGOxsWSxsWazs2Wysmaysmi0tGezs2izs2q1tWm0tGu1tW63t222tm62tnC4uG+3t3C3t3G4uHK4uHG3t3S6unO5uXa7u3W6une7u3m8vHy+vn6/v32+vn6+vn+/v4LAwIbDw4vFxYzFxY7Hx43GxpDIyJHIyJDHx5LIyJXKypjMzJfLy5nMzJzOzpvNzZ3OzqDQ0J/Pz6DPz6PR0aLQ0KTR0abT06XS0qbS0qvV1arU1K3W1qzV1a/X167W1rDX17LY2LTa2rfb27ba2rfa2rnc3Ljb27ze3rvd3bzd3cDg4L/f38Hg4MPh4cTi4sXi4sjk5Mfj48bi4srl5cvl5crk5M/o6M7n583m5s/n59Ho6NDn59Pp6dLo6NTp6dbr69js7Nfr69rt7dns7Nzu7tvt7d7v793u7tzt7eDw8N/v797u7uDv7+Lx8eHw8OTy8uLw8OTx8eby8ur19en09Ojz8+z29uv19e329u/39/L5+fH4+PP5+fb7+/X6+vj8/Pf7+/n8/Pz+/vv9/f7///3+/v7+/v///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAOMALAAAAAAyADIAAAj/ADtsGEiwoMGDCBMqJCiwg8OHECNKnEix4sOBFjNq3OgQI8ePIDtuCElyo8eSKCeeTMlSZMuXAkfCZLnyoYQHOC08BEHhQQUQECE84KDhQQSHPI06FJpBpUyIGp6MCfNlRQWHGXqMAdJUoIcsY0ysGONEQ4cMPsYYkUDhypgYFyTW3CBBVoC7wGroPLAowKMEDjWUiBZAiJYAryB0MMAogLUdZMAFkNNA7tOOEmAF2PMoABwFHQ4gCtAIcAfBzAIEwRJglWIDie4ee3YXTuWIcyW8CkCMthzQokkfGIk6AJAqrV/HvsvctmXcugPYOpVtGAkNwRcRiCu4WQAdUQK0/1IegNY2aMQ834aYW3OfMtS+tbAQHBqtQRA2UKgVwFSsAJuABlsAguRhBinqPcdeXcwFoMhRfDH3CQIdQCCGNndBI8QEizXGyAAH7EaZglBJFUYYTFgwUlZjtEhEXBUuUYggOUAAFFpj+GCBBk68BSN7l9mE0wMSyJQUThMA5VAEBiRwFVI9UQBUBA90hVuQM4VU01INXKAkBw0ooAAEHEAEwgQNpNlAmTtVkKZZFG2pQRdw2NCUBirAQYccVligE1ZTwCGoGyjA2YEFQMARR6FxBrnBBLoE8EZlE/jQYCjXOdSAJw0CcZSmo9xFxwKN4jYBLQGkQWkP3XTjijUOgv/WQQOctHaIISzAaEENsAYwynpXmoqqqh1M0IM300BwRgC/EDVrrdUk84sLfyaQRwDLYIONDBg4JWyqq3ojzQd4BLBLBmXSGoAvo3iSq0AQpBJAKccEoIdpwbJ3KrjFsvpNMOIEAMht6tqCySV6dVBBDtk06BqWc02ASwBsUPpDwAFs84gGcDYASoNHHJUAIAHw4gYf4ajGYb5QQTHGC9ydAMYYYgThgKEXINHiGF6YYFYGTZjhQwEOWFEGDD9ehGUHN/3IwZArQzTBkEMhhWaSFT5gKJBZprRlBxFAsLVDHEAAwZ8PUWD22RFp0ECTinkbkQdNUIECmx2NEAUVNfz/eMEOVFAhxQtRa+CBHITwwcXSNWlAgjIBOCEBRA+EcZco6yUgCXPOJLHyBJowN0vcLAdGQjGRT/5QApfcdQ0M1VISwCV9OGiAwjuE040jjbRBOtdQkWBM6g9lkMIz3VATgB2kdpDAJAEg00sAhtxOwQ3dmJvJFlED/5Djww9RQMdqRN9JAKg44NDzATjDSjXV7ECBwmuEMkwA3PAwf+mnkVDvLalQAocU8IkGaQMHV2FfJYqQiwBYQTETQMMfAoGNAFzhdy4J3jKYIwYGWIAG2YNEH4QRAD8AxgCWaJAqQqCBDUSAF8zRhQjGFhPceKAMgoJDCy5wgRnUQQ0REAARW+AghaZYoAmCmkMZSKCTkdwhEpU4xA32xz+HOEBNcNKAAhqwgTM1AGteTJPWINKABCggAVKSW9dI8rU1aqSNbrQIHONYKjp+ZI52rGIe67jHjIxkIYAMpCAxEhAAOw==
@@ -272,11 +298,7 @@ def sendEmail():
 	html_content = html_content.replace('[TITLE]', 'Daily Nodealyzer Report')
 	html_content = html_content.replace('[SUBTITLE]', config.get('ServerInfo','friendly_server_name'))
 	html_content = html_content.replace('[PANEL]', 'Eventually, a high-level summary will go here.')
-	html_content = html_content.replace('[BODY]', """
-		<p>Hi!<br>
-		Something much more useful will go here eventually.<br>
-		Here is a <a href="http://www.jeremyblum.com">link</a>.</p>
-		""")
+	html_content = html_content.replace('[BODY]', body)
 	html_content = html_content.replace('[FOOTER]', 'Generated by <a href="https://www.github.com/sciguy14/Nodealyzer" title="Nodealyzer on GitHub">Nodealyzer</a>')
 	
 	payload, mail_from, rcpt_to, msg_id=pyzmail.compose_mail(\
